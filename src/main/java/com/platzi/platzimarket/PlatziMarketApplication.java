304c90aa88145a3cfdcba4c14a5ff556eb644822
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-21 17:00:02 -0500

    Creation of a new rest controller class

--- src/main/java/com/platzi/platzimarket/PlatziMarketApplication.java
+++ src/main/java/com/platzi/platzimarket/PlatziMarketApplication.java
@@ -3,7 +3,8 @@ package com.platzi.platzimarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.platzi.platzimarket.persistence.mapper.ProductMapper"})// if it is necessary because the code did not find the mapper so I had to show it.
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
