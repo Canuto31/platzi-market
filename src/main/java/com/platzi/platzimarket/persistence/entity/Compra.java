f0665d6e2590f74ef128aae59c3b40cbad9ae56b
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-21 08:21:58 -0500

    Creation of Producto and Compra entities

---
+++ src/main/java/com/platzi/platzimarket/persistence/entity/Compra.java
@@ -0,0 +1,35 @@
+package com.platzi.platzimarket.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "compras")
+@Data
+public class Compra {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+    
+    @Column(name = "id_cliente")
+    private String idCliente;
+    
+    private LocalDateTime fecha;
+    
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+
+    private String estado;
+    
+}
