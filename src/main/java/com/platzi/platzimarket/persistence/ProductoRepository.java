8eb9774ed81579272cee8a7950657862d98c5f19
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-21 09:54:50 -0500

    How to work the query methods

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.platzi.platzimarket.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
@@ -10,5 +11,13 @@ public class ProductoRepository {
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
